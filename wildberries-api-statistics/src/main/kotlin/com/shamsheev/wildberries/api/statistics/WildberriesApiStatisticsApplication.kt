80df9328a90a17dbeef984ae2587fde5532525a7
Stanislav Shamsheev <sashamsheev@sberbank.ru>
2024-01-20 18:57:57 +0700

    swagger

--- wildberries-api-statistics/src/main/kotlin/com/shamsheev/wildberries/api/statistics/WildberriesApiStatisticsApplication.kt
+++ wildberries-api-statistics/src/main/kotlin/com/shamsheev/wildberries/api/statistics/WildberriesApiStatisticsApplication.kt
@@ -3,11 +3,13 @@ package com.shamsheev.wildberries.api.statistics
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.boot.web.servlet.ServletComponentScan
+//import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
 @ServletComponentScan
+//@EnableFeignClients
 class WildberriesApiStatisticsApplication
 
 fun main(args: Array<String>) {
-	runApplication<WildberriesApiStatisticsApplication>(*args)
+    runApplication<WildberriesApiStatisticsApplication>(*args)
 }
